6f36f8639153c2c63866db8fb452816b22c8a7e6
GODHANIYA AXAY <87019608+godhaniyaakshay@users.noreply.github.com>
2021-07-13 11:55:23 +0530

    Add basic.s with all basic javascript

---
+++ basic.js
@@ -0,0 +1,113 @@
+// console.log("hello world");
+//var,let,const-keyword
+
+//1.var
+
+var a;  //declaration
+a = 10;   //initialization
+
+
+
+//2.let
+
+let b;  //declaration
+b = 10; //initialization
+
+
+
+//3.const
+
+const c = 10; // declaration and initialization at a time
+
+
+//re-declaration and re-initialization
+
+//1.var 
+//re-declaration
+//re-initialization
+
+
+
+//2.let 
+//re-declaration not allow
+//re-initialization
+
+
+
+
+
+//3.const
+//re-declaration not allow
+//re-initialization not allow
+
+
+
+//scope - availability of variable
+
+
+//var - not effected by scope
+//let - effected by scope
+//const - effected by scope
+ 
+{
+  var k=10;
+  console.log (k)
+}
+//hosting - use variable before decalreation
+
+//var - not effected by hosting
+//let - effected by hosting
+//const - effected by hosting
+
+
+
+//datatype
+
+//srting
+//array
+//object
+//boolean
+//null
+//int
+//float
+//double
+//char
+
+//1.srting - set of charecter
+
+// declaration - ""  ,  ''  ,  ``
+// .length - return length of an string
+// .slice(start,end) - trim the string from start to end
+
+
+
+
+//2.array - set of element - list of element
+
+// declaration - []
+// ittrate by [index]
+//.length - return length of an array
+//.push() - add element at the end of an array
+//.pop() - remove element from end of the aray
+//.unshift() - add element to the starting of the array
+// .shift() - remove element from the starting of an array
+//.slice(start,end) - trim the string from start to end
+//.splice(start, howmany) - give the element from array
+
+
+
+//3.object - set of element with key and value
+
+//declaration - {}
+//ittrate by ".key"
+
+
+
+
+
+
+
+
+
+
+
